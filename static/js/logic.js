043518b3a3287afbc252e2f0989d0a6e197a2daa
Jason Butt <jasonbutt13@gmail.com>
2022-09-08 20:16:04 +1000

    updating js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,43 +1,49 @@
 // Create the tile layer that will be the background of our map
-var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
-  id: "light-v10",
+  id: "dark-v10",
   accessToken: API_KEY
 });
 
 // Initialize all of the LayerGroups we'll be using
 var layers = {
-  COMING_SOON: new L.LayerGroup(),
-  EMPTY: new L.LayerGroup(),
-  LOW: new L.LayerGroup(),
-  NORMAL: new L.LayerGroup(),
-  OUT_OF_ORDER: new L.LayerGroup()
+  Theropods: new L.LayerGroup(),
+  Sauropods: new L.LayerGroup(),
+  Stegosaurs: new L.LayerGroup(),
+  Ankylosaurs: new L.LayerGroup(),
+  Ornithopods: new L.LayerGroup(),
+  Ceratopsians: new L.LayerGroup(),
+  Pachycephalosaurs: new L.LayerGroup()
 };
 
 // Create the map with our layers
 var map = L.map("map-id", {
-  center: [40.73, -74.0059],
-  zoom: 12,
+  center: [-37.81, 144.95],
+  zoom: 6,
   layers: [
-    layers.COMING_SOON,
-    layers.EMPTY,
-    layers.LOW,
-    layers.NORMAL,
-    layers.OUT_OF_ORDER
+    layers.Theropods,
+    layers.Sauropods,
+    layers.Stegosaurs,
+    layers.Ankylosaurs,
+    layers.Ornithopods,
+    layers.Ceratopsians,
+    layers.Pachycephalosaurs
   ]
 });
 
 // Add our 'lightmap' tile layer to the map
-lightmap.addTo(map);
+darkmap.addTo(map);
 
 // Create an overlays object to add to the layer control
 var overlays = {
-  "Coming Soon": layers.COMING_SOON,
-  "Empty Stations": layers.EMPTY,
-  "Low Stations": layers.LOW,
-  "Healthy Stations": layers.NORMAL,
-  "Out of Order": layers.OUT_OF_ORDER
+  "Theropods": layers.Theropods,
+  "Sauropods": layers.Sauropods,
+  "Stegosaurs": layers.Stegosaurs,
+  "Ankylosaurs": layers.Ankylosaurs,
+  "Ornithopods": layers.Ornithopods,
+  "Ceratopsians": layers.Ceratopsians,
+  "Pachycephalosaurs": layers.Pachycephalosaurs
 };
 
 // Create a control for our layers, add our overlay layers to it
