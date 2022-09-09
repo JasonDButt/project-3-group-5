5363514c0c2f1d50bcda1ec800e8fe7c9c01f719
Jason Butt <jasonbutt13@gmail.com>
2022-09-09 14:53:58 +1000

    removed empty folder

--- static/js/logic.js
+++ static/js/logic.js
@@ -64,31 +64,43 @@ info.addTo(map);
 
 // Initialize an object containing icons for each layer group
 var icons = {
-  COMING_SOON: L.ExtraMarkers.icon({
+  Theropods: L.ExtraMarkers.icon({
     icon: "ion-settings",
     iconColor: "white",
     markerColor: "yellow",
     shape: "star"
   }),
-  EMPTY: L.ExtraMarkers.icon({
+  Sauropods: L.ExtraMarkers.icon({
     icon: "ion-android-bicycle",
     iconColor: "white",
     markerColor: "red",
     shape: "circle"
   }),
-  OUT_OF_ORDER: L.ExtraMarkers.icon({
+  Stegosaurs: L.ExtraMarkers.icon({
     icon: "ion-minus-circled",
     iconColor: "white",
     markerColor: "blue-dark",
     shape: "penta"
   }),
-  LOW: L.ExtraMarkers.icon({
+  Ankylosaurs: L.ExtraMarkers.icon({
     icon: "ion-android-bicycle",
     iconColor: "white",
     markerColor: "orange",
     shape: "circle"
   }),
-  NORMAL: L.ExtraMarkers.icon({
+  Ornithopods: L.ExtraMarkers.icon({
+    icon: "ion-android-bicycle",
+    iconColor: "white",
+    markerColor: "green",
+    shape: "circle"
+  }),
+  Ceratopsians: L.ExtraMarkers.icon({
+    icon: "ion-android-bicycle",
+    iconColor: "white",
+    markerColor: "green",
+    shape: "circle"
+  }),
+  Pachycephalosaurs: L.ExtraMarkers.icon({
     icon: "ion-android-bicycle",
     iconColor: "white",
     markerColor: "green",
